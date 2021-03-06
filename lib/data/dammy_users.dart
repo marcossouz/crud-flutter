cc8d438ff1f078fe6a9cc911ee09fa57e6d21e6c
Marcos Souza <mrcsz.m@gmail.com>
2021-03-06 16:19:15 -0300

    flutter CRUD

---
+++ lib/data/dammy_users.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  "1": const User(
+    id: '1',
+    name: 'Maria',
+    email: 'maria@alunos.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2014/03/24/17/19/teacher-295387_960_720.png'
+  ),
+  "2": const User(
+      id: '2',
+      name: 'Rafael',
+      email: 'rafael@alunos.com.br',
+      avatarUrl: 'https://cdn.pixabay.com/photo/2014/04/03/10/32/businessman-310819_960_720.png'
+  ),
+  "3": const User(
+      id: '3',
+      name: 'Fernanda',
+      email: 'fernanda@alunos.com.br',
+      avatarUrl: 'https://cdn.pixabay.com/photo/2014/04/03/10/32/user-310807_960_720.png'
+  ),
+  "4": const User(
+      id: '4',
+      name: 'Guilherme',
+      email: 'guilherme@alunos.com.br',
+      avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png'
+  )
+};
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_crud/routes/app_routes.dart';
+import 'package:flutter_crud/views/user_form.dart';
+import 'package:flutter_crud/views/user_list.dart';
+import 'package:flutter_crud/provider/users.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(providers: [
+      ChangeNotifierProvider(
+        create: (ctx) => Users(),
+      )
+    ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        routes: {
+          AppRoutes.HOME: (_) => UserList(),
+          AppRoutes.USER_FORM: (_) => UserForm()
+        },
+      ),
+    );
+  }
+}
